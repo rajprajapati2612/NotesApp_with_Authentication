750523d79180360ca626a734b49dd8458fdbc736
rajprajapati2612 <rajprajapaticode85@gmail.com>
2026-04-07 00:20:18 +0530

    Added changepassword and uservalidators

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,16 +1,18 @@
 import express from "express"
-import { forgotPassword, loginUser, logoutUser, registerUser, verification, verifyOTP } from "../controllers/userController.js";
+import { changePassword, forgotPassword, loginUser, logoutUser, registerUser, verification, verifyOTP } from "../controllers/userController.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
+import { userSchema, validateUser } from "../validators/userValidate.js";
 
 
 const router =  express.Router();
 
 
-router.post("/register",registerUser);
+router.post("/register",validateUser(userSchema),registerUser);
 router.post("/verify",verification);
 router.post("/login",loginUser);
 router.post("/logout",isAuthenticated,logoutUser);
 router.post("/forgot-password",forgotPassword);
-router.post("/verify-otp",verifyOTP);
+router.post("/verify-otp/:email",verifyOTP);
+router.post('/change-password/:email',changePassword);
 
 export default router
